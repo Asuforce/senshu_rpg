413bc8b41c2855987f45e2fd530bcf4b5b220d64
Shun Nishitsuji <asuforcegt@hotmail.co.jp>
2015-03-01 18:23:42 +0900

    ver 0.4

--- RPG.js
+++ RPG.js
@@ -43,66 +43,69 @@ function init() {
         { src: "./pic/chara.png", type:"image", id: "character" }
     ];
 
-    queue.addEventListener("progress", handleProgress);
+    queue.addEventListener("progress", progressLoad);
     queue.loadManifest(manifest);                              //データを取得するときはquere.getResult()
-    queue.addEventListener("complete", handleComplete);      //loadManifestが終わったら実行される
+    queue.addEventListener("complete", mapLoad);      //loadManifestが終わったら実行される
 }
 
 //画像ファイルロードの進捗をパーセンテージで表わす
-function handleProgress(event){
+function progressLoad(event){
     message.text = "Now Loading... " + Math.floor(event.progress * 100)+ "%";
     stage.update();
 }
 
-function handleComplete(event){
-    floorY = 44;
+function mapLoad(event){
+    if (firstload) {
+        floorY = 44;
 
-    mapData = firstMapData;
-    mapLowLayerData = firstMapLowLayerData;
-    mapUpperLayerData = firstMapUpperLayerData;
-    mapObstacleData = firstMapObstacleData;
+        mapData = firstMapData;
+        mapLowLayerData = firstMapLowLayerData;
+        mapUpperLayerData = firstMapUpperLayerData;
+        mapObstacleData = firstMapObstacleData;
 
-    //一番下にくるマップ用スプライトシート作成
-    var floor_SpriteSheet = new createjs.SpriteSheet({
-        images: [queue.getResult("floor")],
-        frames: { width: 32, height: 32 }
-    });
-    floor_SpriteSheetField = new createjs.BitmapAnimation(floor_SpriteSheet);
+        //一番下にくるマップ用スプライトシート作成
+        var floor_SpriteSheet = new createjs.SpriteSheet({
+            images: [queue.getResult("floor")],
+            frames: { width: 32, height: 32 }
+        });
+        floor_SpriteSheetField = new createjs.BitmapAnimation(floor_SpriteSheet);
 
-    //マップ上に配置するチップ用スプライトシート作成
-    var object_SpriteSheet = new createjs.SpriteSheet({
-        images: [queue.getResult("object")],
-        frames: { width: 32, height: 32 }
-    });
-    object_SpriteSheetField = new createjs.BitmapAnimation(object_SpriteSheet);
+        //マップ上に配置するチップ用スプライトシート作成
+        var object_SpriteSheet = new createjs.SpriteSheet({
+            images: [queue.getResult("object")],
+            frames: { width: 32, height: 32 }
+        });
+        object_SpriteSheetField = new createjs.BitmapAnimation(object_SpriteSheet);
 
-    //操作キャラアニメーション用のスプライトシートを作成
-    charaSpriteSheet = new createjs.SpriteSheet({
-        images: [queue.getResult("character")],
-        frames: { width:32, height:32 },
-        animations: {
-            down: { frames: [1, 0, 2], frequency: 10 },
-            up: { frames: [4, 3, 5], frequency: 10 },
-            right: { frames: [6, 7], frequency: 10 },
-            left: { frames: [8, 9], frequency: 10 }
-        }
-    });
-    character = new createjs.BitmapAnimation(charaSpriteSheet);
+        //操作キャラアニメーション用のスプライトシートを作成
+        charaSpriteSheet = new createjs.SpriteSheet({
+            images: [queue.getResult("character")],
+            frames: { width:32, height:32 },
+            animations: {
+                down: { frames: [1, 0, 2], frequency: 10 },
+                up: { frames: [4, 3, 5], frequency: 10 },
+                right: { frames: [6, 7], frequency: 10 },
+                left: { frames: [8, 9], frequency: 10 }
+            }
+        });
+        character = new createjs.BitmapAnimation(charaSpriteSheet);
 
-    //アニメーションさせるキャラの最初の座標を設定
-    character.x = 224;
-    character.y = 224;
+        //アニメーションさせるキャラの最初の座標を設定
+        character.x = 224;
+        character.y = 224;
 
-    //30FPSでスタート
-    createjs.Ticker.setFPS(30);
-    createjs.Ticker.addListener(this);
+        //30FPSでスタート
+        createjs.Ticker.setFPS(30);
+        createjs.Ticker.addListener(this);
 
-    //キーが押された時のイベントリスナーの登録
-    document.addEventListener('keydown', handleKeyDown, false);
-    //キーが離された時のイベントリスナーの登録
-    document.addEventListener('keyup', handleKeyUp, false);
+        //キーが押された時のイベントリスナーの登録
+        document.addEventListener('keydown', handleKeyDown, false);
+        //キーが離された時のイベントリスナーの登録
+        document.addEventListener('keyup', handleKeyUp, false);
 
-    stage.removeChild(message);
+        stage.removeChild(message);
+        firstload = false;
+    }
 
     //一番下にくる背景マップ作成
     var x = 0, y = 0;
@@ -157,6 +160,72 @@ function handleComplete(event){
 
     stage.addChild(character);
     character.gotoAndPlay("down");
+}
+
+function changeFloor(cnt) {
+    stage.removeAllChildren();
+    backgroundMap.removeAllChildren();
+    foregroundMap.removeAllChildren();
+    thisfloor += cnt;
+
+    switch(thisfloor) {
+      case 1:
+        floorY = 44;
+        mapData = firstMapData;
+        mapLowLayerData = firstMapLowLayerData;
+        mapUpperLayerData = firstMapUpperLayerData;
+        mapObstacleData = firstMapObstacleData;
+        break;
+
+      case 2:
+        floorY = 38;
+        mapData = secondMapData;
+        mapLowLayerData = secondMapLowLayerData;
+        mapUpperLayerData = secondMapUpperLayerData;
+        mapObstacleData = secondMapObstacleData;
+        break;
+
+      case 3:
+        floorY = 22;
+        mapData = thirdMapData;
+        mapLowLayerData = thirdMapLowLayerData;
+        mapUpperLayerData = thirdMapUpperLayerData;
+        mapObstacleData = thirdMapObstacleData;
+        break;
+
+      case 4:
+        floorY = 21;
+        mapData = fourthMapData;
+        mapLowLayerData = fourthMapLowLayerData;
+        mapUpperLayerData = fourthMapUpperLayerData;
+        mapObstacleData = fourthMapObstacleData;
+        break;
+
+      case 5:
+        floorY = 20;
+        mapData = fifthMapData;
+        mapLowLayerData = fifthMapLowLayerData;
+        mapUpperLayerData = fifthMapUpperLayerData;
+        mapObstacleData = fifthMapObstacleData;
+        break;
+
+      case 6:
+        floorY = 13;
+        mapData = sixthMapData;
+        mapLowLayerData = sixthMapLowLayerData;
+        mapUpperLayerData = sixthMapUpperLayerData;
+        mapObstacleData = sixthMapObstacleData;
+        break;
+
+      case 7:
+        floorY = 2;
+        mapData = RMapData;
+        mapLowLayerData = RMapLowLayerData;
+        mapUpperLayerData = RMapUpperLayerData;
+        mapObstacleData = RMapObstacleData;
+        break;
+    }
+    mapLoad();
 }
 
 function position(posX, posY) {
@@ -215,21 +284,21 @@ function tick(){
                 direction = 0;
             }
         } else if (keyFlags[1]) { //↑ w ボタン
-           if (prevDirection != 1) {
-            character.gotoAndPlay("up");
-            prevDirection = 1;
-        }
-        if (charaY > 0 && mapObstacleData[y-1][x] === 0) {
-            direction = 1;
-        }
+            if (prevDirection != 1) {
+                character.gotoAndPlay("up");
+                prevDirection = 1;
+            }
+            if (charaY > 0 && mapObstacleData[y-1][x] === 0) {
+                direction = 1;
+            }
         } else if (keyFlags[2]) { //← a ボタン
-           if (prevDirection != 2) {
-            character.gotoAndPlay("left");
-            prevDirection = 2;
-        }
-        if (charaX >= 4 && mapObstacleData[y][x-1] === 0) {
-            direction = 2;
-        }
+            if (prevDirection != 2) {
+                character.gotoAndPlay("left");
+                prevDirection = 2;
+            }
+            if (charaX >= 4 && mapObstacleData[y][x-1] === 0) {
+                direction = 2;
+            }
         } else if (keyFlags[3]) { //→ d ボタン
             if (prevDirection != 3) {
                 character.gotoAndPlay("right");
